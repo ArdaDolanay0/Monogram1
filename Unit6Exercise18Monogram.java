76a4cf73ccf995e85f510b79635348ea8666bca0
RusselWestbrook0 <doctorardadol@gmail.com>
2016-09-22 13:34:29 -0400

    Add files via upload

---
+++ Unit6Exercise18Monogram.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkg4u.unit.pkg6.exercise.pkg18.monogram;
+import java.util.Scanner;
+/**
+ *
+ * @author ardol7945
+ */
+public class Unit6Exercise18Monogram {
+
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+      System.out.println("Enter First Name");
+// TODO code application logic here
+    }
+    
+}
